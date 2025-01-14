5fff5fbf3e874db3a7eb9d5b334d2a4f31f230bb
mikhail-sereda <mikhail.sereda.dev@yandex.ru>
2025-01-14 15:58:29 +0900

    create model answer

---
+++ model/answer.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from model import Base
+
+
+class Answer(Base):
+    __tablename__ = "answers"
+
+    answerID: Mapped[int] = mapped_column(primary_key=True)
+    answersText: Mapped[str]
+    answerCorrect: Mapped[bool]
+    questionID: Mapped[int] = mapped_column(ForeignKey("questions.questionsID"))
+    question: Mapped["Question"] = relationship(back_populates="answers")
+
+
+    def __repr__(self) -> str:
+        return f"Question(id={self.questionsID!r}, questionsText={self.questionsText!r}, category={self.category!r})"
