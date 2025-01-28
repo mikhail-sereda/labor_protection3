d382847901bdb0f880e4c0dd67097773f402530e
mikhail.sereda <mikhail.sereda.dev@yandex.ru>
2025-01-28 23:24:44 +0900

    create put_widget_answer

--- view/add_question/frame_create_question.py
+++ view/add_question/frame_create_question.py
@@ -21,27 +21,47 @@ class FrameCreateQuestion(ctk.CTkFrame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=10)
 
-        self.frame_choice = ctk.CTkFrame(self, fg_color="red")
-        self.frame_add_question = ctk.CTkFrame(self, fg_color="blue")
-        self.frame_add_answers = ctk.CTkFrame(self, fg_color="white")
+        self.answers = [
+            {"text": "первый", "bu": True, "id": 1},
+            {"text": "второйj", "bu": True, "id": 1},
+        ]
 
-        self.combo_choice_category = ctk.CTkComboBox(self.frame_choice, values=["option 1", "option 2"])
-        self.check_choice_professions = ctk.CTkCheckBox(self.frame_choice, text="option 1")
-        self.button_save = ctk.CTkButton(self.frame_choice,
-                                         text="Сохранить",
-                                         font=("Arial", 20),
-                                         border_width=1,
-                                         fg_color="#63c0f2",
-                                         command=None,)
+        self.frame_choice = ctk.CTkFrame(self)  #  fg_color="red"
+        self.frame_add_question = ctk.CTkFrame(self)
+        self.frame_add_answers = ctk.CTkScrollableFrame(self)
+        self.frame_add_answers.grid_columnconfigure(0, weight=1)
+        self.frame_add_answers.grid_columnconfigure(1, weight=30)
+        self.frame_add_answers.grid_columnconfigure(2, weight=1)
 
-        self.text_question = ctk.CTkTextbox(self.frame_add_question)
+        self.combo_choice_category = ctk.CTkComboBox(
+            self.frame_choice, values=["option 1", "option 2"]
+        )
+        self.check_choice_professions = ctk.CTkCheckBox(
+            self.frame_choice, text="option 1"
+        )
+        self.button_save = ctk.CTkButton(
+            self.frame_choice,
+            text="Сохранить",
+            font=("Arial", 20),
+            border_width=1,
+            fg_color="#63c0f2",
+            command=None,
+        )
+
+        self.text_question = ctk.CTkTextbox(self.frame_add_question, font=("Arial", 18))
 
         self.put_frame()
 
     def put_frame(self):
-        self.frame_choice.grid(column=0, row=0, rowspan=2, sticky="ewns", padx=5, pady=5)
-        self.frame_add_question.grid(column=1, row=0, sticky="ewns", padx=5, pady=5)
-        self.frame_add_answers.grid(column=1, row=1, sticky="ewns", padx=5, pady=5)
+        self.frame_choice.grid(
+            column=0, row=0, rowspan=2, sticky="ewns", padx=5, pady=5
+        )
+        self.frame_add_question.grid(
+            column=1, row=0, sticky="ewns", padx=(0, 5), pady=(5, 5)
+        )
+        self.frame_add_answers.grid(
+            column=1, row=1, sticky="ewns", padx=(0, 5), pady=(0, 5)
+        )
         self.put_widget_choice()
         self.put_widget_question()
         self.put_widget_answer()
@@ -49,10 +69,31 @@ class FrameCreateQuestion(ctk.CTkFrame):
     def put_widget_choice(self):
         self.combo_choice_category.pack()
         self.check_choice_professions.pack()
-        self.button_save.pack(padx=15, pady=10, ipadx=10, ipady=10, fill="x", side="bottom")
+        self.button_save.pack(
+            padx=15, pady=10, ipadx=10, ipady=10, fill="x", side="bottom"
+        )
 
     def put_widget_question(self):
-        self.text_question.pack(fill="x")
+        self.text_question.pack(
+            expand=True, fill="both", ipadx=20, ipady=20, anchor="center"
+        )
 
     def put_widget_answer(self):
-        pass
+        for answer in range(len(self.answers)):
+            chek = ctk.CTkCheckBox(self.frame_add_answers, text="", width=0).grid(
+                column=0, row=answer, padx=5, pady=5
+            )
+            self.text = ctk.CTkTextbox(self.frame_add_answers, height=70)
+            self.text.grid(column=1, row=answer, sticky="ew", padx=5, pady=5)
+            self.text.insert("1.0", self.answers[answer]["text"])
+            btn = ctk.CTkButton(
+                self.frame_add_answers, text="Удалить", height=50, width=50
+            ).grid(
+                column=3,
+                row=answer,
+                columnspan=1,
+                padx=5,
+                pady=5,
+            )
+        for i in self.frame_add_answers.winfo_children():
+            print(i.__dict__)
